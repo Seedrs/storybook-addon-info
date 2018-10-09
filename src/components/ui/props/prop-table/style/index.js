6b59ab328868eda119b5a7a43ebe91652b0af0a7
Rishee Patel <rishee.patel@gmail.com>
2018-10-09 05:31:08 +0100

    feat(package.json): Remove @seedrs/seedrs-ui-colors

--- src/components/ui/props/prop-table/style/index.js
+++ src/components/ui/props/prop-table/style/index.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import { rem, padding, margin, transitions } from 'polished';
-import { colors } from '../../../colors';
 
 export const Table = styled.table`
   ${margin(0, 0, rem('24px'))}
@@ -10,7 +9,7 @@ export const Table = styled.table`
   width: auto;
   border-spacing: 0;
   overflow: hidden;
-  border: ${rem('1px')} solid ${colors.neutral.midlight};
+  border: ${rem('1px')} solid #DFE3E8;
   border-radius: ${rem('4px')};
 `;
 
@@ -18,7 +17,7 @@ export const Tr = styled.tr`
   &:hover {
     td {
       color: white;
-      background-color: ${colors.yellow.base};
+      background-color: #FFC021;
     }
   }
 
@@ -31,13 +30,13 @@ export const Tr = styled.tr`
 
 export const Th = styled.th`
   ${padding(rem('8px'), rem('16px'))}
-  border-left: ${rem('1px')} solid ${colors.neutral.midlight};
-  border-bottom: ${rem('1px')} solid ${colors.neutral.midlight};
+  border-left: ${rem('1px')} solid #DFE3E8;
+  border-bottom: ${rem('1px')} solid #DFE3E8;
   font-size: ${rem('14px')};
   font-weight: 700;
   text-align: left;
-  color: ${colors.neutral.dark};
-  background-color: ${colors.neutral.light};
+  color: #454F5B;
+  background-color: #F4F6F8;
 
   &:first-child {
     border-left: none;
@@ -47,10 +46,10 @@ export const Th = styled.th`
 export const Td = styled.td`
   ${padding(rem('8px'), rem('16px'))}
   ${transitions('background-color 200ms ease-in-out', 'color 200ms ease-in-out')}
-  border-left: ${rem('1px')} solid ${colors.neutral.xlight};
-  border-top: ${rem('1px')} solid ${colors.neutral.light};
+  border-left: ${rem('1px')} solid #F9FAFB;
+  border-top: ${rem('1px')} solid #F4F6F8;
   font-size: ${rem('14px')};
-  color: ${colors.neutral.dark};
+  color: #454F5B;
   background-color: white;
 
   &:first-child {
